5e111fc70b724011ad4c3ce4bb7d0e3e2faa26d6
Sunny Kotwal <32493215+kotsun32@users.noreply.github.com>
2022-04-17 15:56:30 -0400

    Add files via upload

---
+++ create_model.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import *
+
+training_data_df = pd.read_csv("sales_data_training_scaled.csv")
+
+X = training_data_df.drop('total_earnings', axis=1).values
+Y = training_data_df[['total_earnings']].values
+
+# Define the model
+model = Sequential()
+# initial layer so input_dim is needed
+model.add(Dense(50, input_dim=9, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+# final output layer
+model.add(Dense(1, activation='linear'))
+# typing mse also works
+model.compile(loss="mean_squared_error", optimizer="adam")
